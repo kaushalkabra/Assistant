07c0c6036fe6ce6c691b19137785156f8944aa50
kaushalkabra <kaushalkabra7@gmail.com>
2018-02-19 19:55:09 -0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,119 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+const FBSDK = require('react-native-fbsdk');
+const {
+  LoginButton,
+  LoginManager,
+  AccessToken,
+  GraphRequestManager,
+  GraphRequest
+} = FBSDK;
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu....',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu....',
+});
+
+export default class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      result:'ab'
+    }
+  }
+ 
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+          {this.state.result}
+        </Text>
+        <LoginButton
+          publishPermissions={["publish_actions"]}
+          onLoginFinished={
+            (error, result) => {
+              if (error) {
+                alert("login has error: " + result.error);
+              } else if (result.isCancelled) {
+                alert("login is cancelled.");
+              } else {
+                AccessToken.getCurrentAccessToken().then(
+                  (data) => {
+                    let accessToken = data.accessToken;
+                    const responseInfoCallback = (error, result) => {
+                      if (error) {
+                        console.log(error)
+                        alert('Error fetching data: ' + error.toString());
+                      } else {
+                        console.log(result)
+                        alert('Success fetching data: ' + result.toString());
+                      }
+                    }
+        
+                    const infoRequest = new GraphRequest(
+                      '/me',
+                      {
+                        accessToken: accessToken,
+                        parameters: {
+                          fields: {
+                            string: 'email,name,first_name,middle_name,last_name'
+                          }
+                        }
+                      },
+                      responseInfoCallback
+                    );
+        
+                    // Start the graph request.
+                    new GraphRequestManager().addRequest(infoRequest).start();
+                  }
+                )
+              }
+            }
+          }
+          onLogoutFinished={() => alert("logout.")}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
